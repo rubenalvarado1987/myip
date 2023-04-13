0f4be729dcb50102da77cd6885b814fb1cb6628c
Ruben Alvarado <ruben.alvarado0@walmart.com>
2023-04-13 13:33:48 -0400

    create endpoint test listen

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ console.log("puerto",process.env.PORT)
 
 const express = require('express')
 const app = express()
+const port = 8080
 
 app.get('/healtz', (req, res) => {
     res.send("ok")
@@ -14,3 +15,8 @@ app.get('/', (req, res) => {
     res.send("ok")
     console.log(`/`);
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening on ports ${port}`)
+    //console.log(req.header('x-forwarded-for'))
+  })
