5c248e90ac74b3b648c5904d845cf7619e87c4df
Ruben Alvarado <ruben.alvarado0@walmart.com>
2023-04-13 12:40:21 -0400

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+
 console.log(process.env.PORT)
+
+
+const express = require('express')
+const app = express()
+
+app.get('/healtz', (req, res) => {
+    res.send("ok")
+    console.log(`healtz ok`);
+});
+
+app.get('/', (req, res) => {
+    res.send("ok")
+    console.log(`/`);
+});
